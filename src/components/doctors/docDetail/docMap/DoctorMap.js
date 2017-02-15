782464fb5d28f32dd80d5eb689441c31870958ab
Saul Flores <saulpolo95@gmail.com>
2017-02-15 14:45:16 -0800

    Passed Map Props

--- src/components/doctors/docDetail/docMap/DoctorMap.js
+++ src/components/doctors/docDetail/docMap/DoctorMap.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import styles from './DoctorMap.css'
-const position = [51.505, -0.09]
+const position = [32.5194358, -117.0101997]
+import L from 'leaflet'
 
-const DoctorMap = () => (
+var DoctorMapIcon = L.icon({
+  iconUrl: 'https://s28.postimg.org/ohtzb6h1p/Doctores.png',
+//iconSize: [80, 80],
+// iconAnchor: [38, 38],
+popupAnchor: [18, 0],
+//  shadowUrl: '',
+//  shadowSize: [68, 95],
+//  shadowAnchor: [22, 94]
+})
+
+const DoctorMap = (doctor) => (
   <div className={styles.map}>
-    <Map center={position} zoom={13}>
-      <TileLayer
-        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          <span>A pretty CSS3 popup</span>
-        </Popup>
-      </Marker>
-    </Map>
+    <Map center={position} zoom={15}>
+        <TileLayer
+          attribution='<a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker icon={DoctorMapIcon} position={position}>
+          <Popup>
+            <span>{doctor.name}</span>
+          </Popup>
+        </Marker>
+      </Map>
   </div>
 )
 
