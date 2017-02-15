3fc78b12d74061a7aad647b95f46d0fa4253c8dd
Saul Flores <saulpolo95@gmail.com>
2017-02-15 14:41:39 -0800

    Added Map

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -4,9 +4,6 @@ import { Switch } from 'react-router'
 
 import About from '../about/About'
 import NotFound from '../notfound/NotFound'
-import Home from '../home/Home'
-import PostDetail from '../postDetail/PostDetail'
-import posts from '../../../blog-posts.json'
 
 import doctors from '../../../static_db/doctors.json'
 import DoctorWrapper from '../doctors/docWrapper/DoctorWrapper'
@@ -14,7 +11,7 @@ import DoctorDetail from '../doctors/docDetail/DoctorDetail'
 
 const Routes = () => (
   <Switch>
-    <Route exact path='/' component={() => <Home posts={posts.posts} />} />
+    <Route exact path='/' component={() => <DoctorWrapper doctors={doctors} />} />
     <Route exact path='/doctors' component={() => <DoctorWrapper doctors={doctors} />} />
     <Route exact path='/doctors/:id' component={props => {
       const doctor = doctors.filter(doctor => props.match.params.id === doctor._id)
@@ -22,10 +19,6 @@ const Routes = () => (
       return <DoctorDetail doctor={doctor[0]} />
     }} />
     <Route exact path='/about' component={About} />
-    <Route exact path='/post/:slug' component={props => {
-      const post = posts.posts.filter(post => props.match.params.slug === post.slug)
-      return <PostDetail post={post[0]} />
-    }} />
     <Route component={NotFound} />
   </Switch>
 )
