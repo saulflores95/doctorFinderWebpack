ad108a7cebf5ee52f13b967571a16b51fb99964e
Luis Gerardo <luigii_1506@hotmail.com>
2017-03-01 16:11:06 -0800

    Animation added

--- src/components/doctors/docWrapper/DoctorWrapper.js
+++ src/components/doctors/docWrapper/DoctorWrapper.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './DoctorWrapper.css'
 import DocCatList from './docCatList/DocCatList'
 import LoadingComponent from '../../MainLayout/loadingComponent/LoadingComponent'
+import { RouteTransition } from 'react-router-transition'
 
 const doctorCategory = (props) => {
   var arr = props.map((doctor) => {
@@ -15,9 +16,17 @@ const doctorCategory = (props) => {
 
 const DoctorWrapper = ({doctors}) => (
   <div className={styles.wrapper}>
-    {doctors.length
-    ? doctorCategory(doctors).map(specialty => <DocCatList specialty={specialty} />) : <LoadingComponent />
-    }
+    <RouteTransition
+      pathname={`/doctors`}
+      atEnter={{ translateX: 35, opacity: 0, 'animation-duration': '10s' }}
+      atLeave={{ translateX: -35, opacity: 0, 'animation-duration': '10s' }}
+      atActive={{ translateX: 0, opacity: 1, 'animation-duration': '10s' }}
+      mapStyles={styles => ({ transform: `translateX(${styles.translateX}%)`, opacity: styles.opacity, 'animation-duration': styles.transition })}
+      >
+      {doctors.length
+      ? doctorCategory(doctors).map(specialty => <DocCatList specialty={specialty} />) : <LoadingComponent />
+      }
+    </RouteTransition>
   </div>
 )
 
--- src/components/doctors/docWrapper/docListWrapper/DocListWrapper.js
+++ src/components/doctors/docWrapper/docListWrapper/DocListWrapper.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import DoctorList from './docList/DoctorList'
 import styles from '../DoctorWrapper.css'
+import { RouteTransition } from 'react-router-transition'
 
 const DoctorWrapper = ({doctor}) => (
   <div className={styles.wrapper}>
-    {doctor.length
-    ? doctor.map(doctor => <DoctorList doctor={doctor} />) : <p>Awaiting Doctors</p>
-    }
+    <RouteTransition
+      pathname={`/doctors/${doctor.specialty}/${doctor._id}`}
+      atEnter={{ translateY: 50, opacity: 0 }}
+      atLeave={{ translateY: -50, opacity: 0 }}
+      atActive={{ translateY: 0, opacity: 1 }}
+      mapStyles={styles => ({ transform: `translateY(${styles.translateY}%)`, opacity: styles.opacity })}
+      >
+      {doctor.length
+      ? doctor.map(doctor => <DoctorList doctor={doctor} />) : <p>Awaiting Doctors</p>
+      }
+    </RouteTransition>
   </div>
 )
 
