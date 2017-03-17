c1d0ebc9db8b1e102d0cefb04e4d41337134414f
Luis Gerardo <luigii_1506@hotmail.com>
2017-03-17 10:53:39 -0700

    UpperNavigationFilter added

--- src/components/MainLayout/UpperNavigation/UpperNavigationFilter.js
+++ src/components/MainLayout/UpperNavigation/UpperNavigationFilter.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import NewUpperNavigation from './newUpperNavigation/NewUpperNavigation'
-import doctors from '../../../../static_db/doctors.json'
 
 const UpperNavigationFilter = () => {
   var doctorString = '/doctors/'
   var path = location.pathname
-  console.log(path)
-  console.log(doctorString)
-  if (location.pathname === '/laboratories') {
+  if (path === '/laboratories') {
     return null
-  }
-  else if (path.indexOf( doctorString ) > -1 ) {
+  } else if (path.indexOf(doctorString) > -1) {
     return null
-  }
-  else {
+  } else {
     return <NewUpperNavigation />
   }
 }
