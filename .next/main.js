d20dc76ba907fb6d429588b535511810c9eebc31
Luis Gerardo <luis.encinas1506@gmail.com>
2017-04-05 19:33:02 -0700

    ButtinNavigation fixed

--- .next/main.js
+++ .next/main.js
@@ -1,15 +1,15 @@
 module.exports =
 webpackJsonp([3],{
 
-/***/ 552:
+/***/ 554:
 /***/ (function(module, exports, __webpack_require__) {
 
+__webpack_require__(232);
 __webpack_require__(231);
-__webpack_require__(230);
-module.exports = __webpack_require__(229);
+module.exports = __webpack_require__(230);
 
 
 /***/ })
 
-},[552]);
+},[554]);
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlUm9vdCI6IiJ9
--- components/MainLayout/BottomNavigation/BottomNavigation.js
+++ components/MainLayout/BottomNavigation/BottomNavigation.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation'
-import IconLocationOn from 'material-ui/svg-icons/communication/location-on'
+import FaMapMarker from 'react-icons/lib/fa/map-marker'
 import FaHospitalO from 'react-icons/lib/fa/hospital-o'
 import FaUserMd from 'react-icons/lib/fa/user-md'
 import FaPlus from 'react-icons/lib/fa/plus'
@@ -21,54 +21,51 @@ export default class ButtomNavigationComponet extends Component {
   }
 
   render () {
+    var styles = {
+      icon: {
+        height: 24,
+        textAlign: 'center'
+      },
+      bottomNavItem: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'block'
+      }
+    }
     return (
       <div className='footer'>
         <MuiThemeProvider>
           <BottomNavigation selectedIndex={this.state.selectedIndex}>
             <Link href='/doctors'>
               <BottomNavigationItem
-                icon={<FaUserMd className='icon' />}
+                icon={<FaUserMd style={styles.icon} />}
                 onClick={() => this.select(0)}
-                className='bottomNavItem'
+                style={styles.bottomNavItem}
               />
             </Link>
             <Link href='/categories'>
               <BottomNavigationItem
-                icon={<FaHospitalO className='icon' />}
+                icon={<FaHospitalO style={styles.icon} />}
                 onClick={() => this.select(1)}
-                className='bottomNavItem'
+                style={styles.bottomNavItem}
               />
             </Link>
             <Link href='/map'>
               <BottomNavigationItem
-                icon={<IconLocationOn className='icon' />}
+                icon={<FaMapMarker style={styles.icon} />}
                 onClick={() => this.select(2)}
-                className='bottomNavItem'
+                style={styles.bottomNavItem}
               />
             </Link>
             <Link href='/contact'>
               <BottomNavigationItem
-                icon={<FaPlus className='icon' />}
+                icon={<FaPlus style={styles.icon} />}
                 onClick={() => this.select(3)}
-                className='bottomNavItem'
+                style={styles.bottomNavItem}
               />
             </Link>
           </BottomNavigation>
         </MuiThemeProvider>
-        <style jsx>
-          {`
-            .icon {
-              text-align: center;
-              font-size: 24px;
-            }
-
-            .bottomNavItem {
-              margin-left: auto !important;
-              margin-right: auto !important;
-              display: block !important;
-            }
-          `}
-        </style>
       </div>
     )
   }
