ed1033b3549d6badbb1d1a6ceb9f4f328c62cd20
Saul Flores <saulpolo95@gmail.com>
2017-05-09 22:49:49 -0700

    Changed to https request in lab image choose

--- components/labs/labsWrapper/labsMainList/LabsMainList.js
+++ components/labs/labsWrapper/labsMainList/LabsMainList.js
@@ -10,7 +10,7 @@ const imageChooser = (props) => {
     return 'https://pbs.twimg.com/profile_images/705493262472056832/i_GpO3EZ.jpg'
   }
   if (props === 'Roma') {
-    return 'https://pbs.twimg.com/media/C8WV4WLW0AMYdpn.jpg'
+    return 'https://s3-us-west-1.amazonaws.com/healthcarebaja/roma.jpg'
   } else {
     console.log('No tag fits the choosen image')
   }
