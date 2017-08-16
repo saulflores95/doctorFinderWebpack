55b523039fdff0a2ffd6b03e36988be878e950c6
Saul Flores <saulpolo95@gmail.com>
2017-08-16 13:06:46 -0700

    Added all data to contact-us

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -3,11 +3,13 @@ import App from '../components/App/App'
 import ContactForm from '../components/contact/ContactForm'
 import BorderWaitTime from '../components/border/borderWaitTime'
 import fetch from 'isomorphic-unfetch'
+import CurrencyValue from '../components/border/CurrencyValue'
 
-const contact = ({ borderWaitTimeJSON }) => (
+const contact = ({ borderWaitTimeJSON, currency }) => (
   <div>
     <App>
       <ContactForm />
+      <CurrencyValue currency={currency} />
       <BorderWaitTime times={ borderWaitTimeJSON }/>
     </App>
   </div>
@@ -16,7 +18,10 @@ const contact = ({ borderWaitTimeJSON }) => (
 contact.getInitialProps = async ({ req }) => {
   const borderWaitTimeResponse = await fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fapps.cbp.gov%2Fbwt%2Fcustomize_rss.asp%3FportList%3D250601%2C250603%2C250401%2C250407%26lane%3Dall%26action%3Drss%26f%3Dhtml')
   const borderWaitTimeJSON = await borderWaitTimeResponse.json()
-  return { borderWaitTimeJSON: borderWaitTimeJSON.items}
+  const currencyValue = await fetch('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.banxico.org.mx%2Frsscb%2Frss%3FBMXC_canal%3Dfix%26BMXC_idioma%3Des')
+  const currency = await currencyValue.json()
+
+  return { borderWaitTimeJSON: borderWaitTimeJSON.items, currency: currency.items[0].title}
 }
 
 
